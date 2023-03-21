dbb35a606615499de737baf2f81d514d25d047ef
sukhrobbeksoatov <suhrobbek_soatov@mail.ru>
2023-03-21 16:29:54 +0500

    add changes

--- js/main.js
+++ js/main.js
@@ -1,33 +1,41 @@
 // Els
-const elTimerText = document.querySelector("[data-timer-text]")
-const elTimerStart = document.querySelector("[data-timer-starter]")
-const elTimerPause = document.querySelector("[data-timer-stopper]")
-
+const elTimerText = document.querySelector("[data-timer-text]");
+const elTimerStart = document.querySelector("[data-timer-starter]");
+const elTimerPause = document.querySelector("[data-timer-stopper]");
 
 // Vars
-let currentTime = 0
-let timerInterval
-let count = true
+let currentTime = 0;
+let timerInterval;
+let count = true;
+
+document.addEventListener("click", (evt) => {
+  start(evt);
+  pause(evt);
+});
 
 // Fn start
-function start() {
+function start(evt) {
+  const el = evt.target.closest("[data-timer-starter]");
+
+  if (!el) return;
+
   if (count) {
     timerInterval = setInterval(() => {
-      currentTime++
-      elTimerText.textContent = currentTime
+      currentTime++;
+      elTimerText.textContent = currentTime;
     }, 1000);
-    count = !count
-    elTimerText.style.color = "lime"
+    count = !count;
+    elTimerText.style.color = "lime";
   }
 }
 
 // Fn pause
-function pause() {
-  clearInterval(timerInterval)
-  count = !count
-  elTimerText.style.color = "black"
+function pause(evt) {
+  const el = evt.target.closest("[data-timer-stopper]");
+
+  if (!el) return;
+
+  clearInterval(timerInterval);
+  count = !count;
+  elTimerText.style.color = "red";
 }
-
-elTimerStart.addEventListener("click", start)
-
-elTimerPause.addEventListener("click", pause)
